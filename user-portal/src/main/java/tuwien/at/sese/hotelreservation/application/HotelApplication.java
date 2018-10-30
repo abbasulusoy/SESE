ca31fc3f73a065de6fac07cf236290fe105e0497
abbas <abbasulusoy@hotmail.com>
2018-10-30 16:00:22 +0100

    copy frontend

--- user-portal/src/main/java/tuwien/at/sese/hotelreservation/application/HotelApplication.java
+++ user-portal/src/main/java/tuwien/at/sese/hotelreservation/application/HotelApplication.java
@@ -11,21 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelApplication {
 
 	public static void main(String[] args) {
-        Connection conn = null;
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println(" Unable to load driver. ");
-        }
-        String url = "jdbc:mysql://db4free.net:3306/hotel2018xp1";
-        String username = "hotel2018xp1";
-        String password = "hotel2018xp1";
-        try {
-            conn = DriverManager.getConnection(url, username, password);
-            System.out.println(" Connection Established. ");
-        } catch (SQLException e) {
-            System.out.println(" Error connecting to database:  " + e);
-        }
 		SpringApplication.run(HotelApplication.class, args);
 	}
 }

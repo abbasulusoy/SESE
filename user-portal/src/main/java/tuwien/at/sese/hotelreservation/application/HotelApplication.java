363293014153917fc52307ceb65f27e1f8e3b1bd
Abbas ULUSOY <abbas.ulusoy@allianz.at>
2018-10-30 14:20:05 +0100

    sql connection

--- user-portal/src/main/java/tuwien/at/sese/hotelreservation/application/HotelApplication.java
+++ user-portal/src/main/java/tuwien/at/sese/hotelreservation/application/HotelApplication.java
@@ -1,5 +1,9 @@
 package tuwien.at.sese.hotelreservation.application;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +11,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelApplication {
 
 	public static void main(String[] args) {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(" Unable to load driver. ");
+        }
+        String url = "jdbc:mysql://db4free.net:3306/hotel2018xp1";
+        String username = "hotel2018xp1";
+        String password = "hotel2018xp1";
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+            System.out.println(" Connection Established. ");
+        } catch (SQLException e) {
+            System.out.println(" Error connecting to database:  " + e);
+        }
 		SpringApplication.run(HotelApplication.class, args);
 	}
 }

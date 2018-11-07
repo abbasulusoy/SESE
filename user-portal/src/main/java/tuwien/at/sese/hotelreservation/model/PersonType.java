e8f1221701566dfe424227fcbf8c63fae7edbe9f
Abbas ULUSOY <abbas.ulusoy@allianz.at>
2018-11-07 11:04:49 +0100

    add new models

---
+++ user-portal/src/main/java/tuwien/at/sese/hotelreservation/model/PersonType.java
@@ -0,0 +1,18 @@
+package tuwien.at.sese.hotelreservation.model;
+
+/**
+ * The Enum  PersonType.
+ */
+public enum PersonType {
+
+    /** The employee. */
+    EMPLOYEE,
+
+    /** The customer. */
+    CUSTOMER,
+
+    /** The admin. */
+    ADMIN,
+    /** The others. */
+    OTHERS;
+}

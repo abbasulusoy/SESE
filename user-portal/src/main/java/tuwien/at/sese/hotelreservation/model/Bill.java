8fb85734a481d904fd86785237411412130bac37
Abbas ULUSOY <abbas.ulusoy@allianz.at>
2018-11-09 09:36:48 +0100

    Hotel, Service Repro USW

--- user-portal/src/main/java/tuwien/at/sese/hotelreservation/model/Bill.java
+++ user-portal/src/main/java/tuwien/at/sese/hotelreservation/model/Bill.java
@@ -17,26 +17,62 @@ public class Bill extends EntityId {
 	private String duration;
     @Column
     private Date billDate;
+
     /**
-    private Room room;
-   
-     * TODO Address 
+     *     private Room room;
+     * TODO Address.
+     *
+     *
+     */
+
+    /**
+     * @return the rabatt
      */
 	public String getRabatt() {
 		return rabatt;
 	}
+
+    /**
+     * Sets the rabatt.
+     *
+     * @param rabatt the new rabatt
+     */
 	public void setRabatt(String rabatt) {
 		this.rabatt = rabatt;
 	}
+
+    /**
+     * Gets the duration.
+     *
+     * @return the duration
+     */
 	public String getDuration() {
 		return duration;
 	}
+
+    /**
+     * Sets the duration.
+     *
+     * @param duration the new duration
+     */
 	public void setDuration(String duration) {
 		this.duration = duration;
 	}
+
+    /**
+     * Gets the bill date.
+     *
+     * @return the bill date
+     */
 	public Date getBillDate() {
 		return billDate;
 	}
+
+    /**
+     * Sets the bill date.
+     *
+     * @param billDate the new bill date
+     */
 	public void setBillDate(Date billDate) {
 		this.billDate = billDate;
 	}
